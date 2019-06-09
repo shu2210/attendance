7f9335ee3f825b72f1365e1de537232e77c6cbbb
Shu <skyscraper2210@yahoo.co.jp>
2019-06-09 17:00:11 +0900

    flash message

--- app/javascript/packs/attendance.js
+++ app/javascript/packs/attendance.js
@@ -1,6 +1,11 @@
 import Vue from 'vue/dist/vue.esm.js'
 import Header from './components/header.vue'
 import Attendance from './components/attendance.vue'
+import VueFlashMessage from 'vue-flash-message'
+
+require('vue-flash-message/dist/vue-flash-message.min.css');
+
+Vue.use(VueFlashMessage)
 
 var app = new Vue({
   el: '#app',

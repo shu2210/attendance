da0fe737aca5d529a426d201713b548eda9645c3
Shu <skyscraper2210@yahoo.co.jp>
2019-06-09 19:36:09 +0900

    fix header

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -1,10 +1,12 @@
 <template>
-  <nav>
-    <div class="nav-wrapper">
-      <router-link to="/" class="brand-logo">勤怠管理</router-link>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><router-link to="/show">勤怠一覧</router-link></li>
-      </ul>
-    </div>
-  </nav>
+  <div>
+    <nav>
+      <div class="nav-wrapper">
+        <router-link to="/" class="brand-logo left">勤怠管理</router-link>
+        <ul class="right">
+          <li><router-link to="/show">一覧</router-link></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
